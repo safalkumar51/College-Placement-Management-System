cc3dd94aa93dfe53cf19e258232a950ef3159336
safalkumar51 <safalp2511@gmail.com>
2025-06-28 19:31:32 +0530

    config changes

--- frontend/src/config/config.jsx
+++ frontend/src/config/config.jsx
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://collegeplacementmanagementsystembe.onrender.com/api/v1';
+export const BASE_URL = 'https://college-placement-management-system-u8py.onrender.com/api/v1';
